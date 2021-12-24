ead8f1059211c88a317de6f7dc46607920bf8eba
syoch <hacksyoch@gmail.com>
2021-12-24 20:17:24 +0900

    update: remove unused functions

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,5 @@
 var wasm_args = [];
-var wasm_func = "";
 
-function wasm_call() {
-  eval(wasm_func)(...wasm_args);
-}
-
-_log = console.log;
 console.log = function () {
   $('#console').append([...arguments].join(" ") + '<br>');
 }
