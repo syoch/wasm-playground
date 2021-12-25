db2510345376d4f92a98b0f2fe86e84cecd927b1
syoch <hacksyoch@gmail.com>
2021-12-25 20:56:06 +0900

    refactor: wip

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,11 @@
 var wasm_args = [];
 
-console.log = function () {
-  $('#console').append([...arguments].join(" ") + '<br>');
+function make_console_hook(name) {
+  return function () {
+    $('#console').append(name + ": " + [...arguments].join(" ") + '<br>');
+  }
 }
+
+console.log = make_console_hook("log");
+console.warn = make_console_hook("warn");
+console.error = make_console_hook("error");
