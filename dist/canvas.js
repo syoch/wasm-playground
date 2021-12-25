bae64613e18e6b7b6e51a865f9099854487a10c7
syoch <hacksyoch@gmail.com>
2021-12-26 07:43:52 +0900

    update: Added get_canvas_size

--- dist/canvas.js
+++ dist/canvas.js
@@ -4,15 +4,17 @@ let canvas;
 /** @type {CanvasRenderingContext2D} */
 let ctx;
 
+$(() => {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext('2d');
+});
+
 function update_canvas_size() {
   canvas.width = canvas.clientWidth;
   canvas.height = canvas.clientHeight;
 }
 
-$(() => {
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext('2d');
-});
+document.addEventListener('resize', update_canvas_size);
 
 /**
  * Draw Rect on canvas
@@ -22,9 +24,16 @@ $(() => {
  * @param {number} d Y coordinate of the second point
  * @param {*} color Fill color
  */
-function DrawRect(a, b, c, d, _color) {
-  console.log("DrawRect", a, b, c, d, _color);
+function draw_rect(a, b, c, d, _color) {
   update_canvas_size();
   ctx.fillStyle = _color.toString();
   ctx.fillRect(a, b, c, d);
+}
+
+/**
+ * Get Canvas size
+ * @returns {number[]} [width, height]
+ */
+function get_canvas_size() {
+  return [canvas.width, canvas.height];
 }
