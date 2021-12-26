1412dfac15489f63352745327851873a3319d2b5
syoch <hacksyoch@gmail.com>
2021-12-26 22:36:25 +0900

    update: impl some

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,23 @@
 #include "canvas.hpp"
 
 extern "C" {
-void EMSCRIPTEN_KEEPALIVE tick() { std::cout << "tick" << std::endl; }
+void EMSCRIPTEN_KEEPALIVE tick() {
+  static int i = 0;
+  i++;
+  if (i % 30 != 0) {
+    return;
+  }
+
+  if (i % 60 == 0) {
+    DrawRect(0, 0, 100, 100, "red");
+  }
+  if (i % 60 == 30) {
+    DrawRect(0, 0, 100, 100, "blue");
+  }
+}
 }
 int main() {
   CallJSFunction("start_canvas_loop");
-  DrawRect(0, 0, 100, 100, "red");
 
-  ClearCanvas();
   return 0;
 }
