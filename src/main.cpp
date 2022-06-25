6cb4453746bde0538db5fcc58691d80bbd8cc303
syoch <syoch64@gmail.com>
2022-06-25 17:26:23 +0900

    edit pattern

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,33 @@
 #include "canvas.hpp"
 
 extern "C" {
-void EMSCRIPTEN_KEEPALIVE tick() {
-  static int y = 0;
+void tick() {
   static int t = 0;
 
-  for (int x = 0; x < canvas::GetWidth(); x++) {
-    canvas::DrawPixel(x, y, (~x ^ y) | t, (x ^ y) | t, (x ^ ~y) | t);
+  // for (int x = 0; x < canvas::GetWidth(); x++) {
+  // canvas::DrawPixel(x, y, (~x ^ y) | t, (x ^ y) | t, (x ^ ~y) | t);
+  // }
+
+  auto image = canvas::GetImageData();
+  auto data = image["data"];
+
+  auto height = canvas::GetHeight();
+  auto width = canvas::GetWidth();
+
+  for (int y = 0; y < height; y++) {
+    for (int x = 0; x < width; x++) {
+      auto offset = 4 * (y * width + x);
+      data.set(offset + 0, (~x ^ y) | t);
+      data.set(offset + 1, (x ^ y) | t);
+      data.set(offset + 2, (x ^ ~y) | t);
+      data.set(offset + 3, 255);
+    }
   }
 
-  y = y + 1;
-  if (y == canvas::GetHeight()) {
-    t = (t + 0x10) & 0xff;
-    y = 0;
-  }
+  canvas::PutImageData(image);
+
+  t += 1;
+  if (t == 256) t = 0;
 }
 }
 
@@ -28,6 +42,5 @@ int main() {
 
   std::cout << "Starting main loop" << std::endl;
   emscripten_set_main_loop(&tick, 1000, 0);
-
   return 0;
 }
