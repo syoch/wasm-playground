e76d6d3f0113e971d5535440ab5642edb11a86ce
syoch <syoch64@gmail.com>
2022-06-25 17:26:12 +0900

    update: Added buffer

--- src/canvas.hpp
+++ src/canvas.hpp
@@ -12,13 +12,11 @@ thread_local val canvas("0");
 
 template <typename T>
 void DrawRect(int x, int y, int w, int h, T color) {
-  if (!ctx.as<bool>()) return;
   ctx.set("fillStyle", std::to_string(color));
   ctx.call<void>("fillRect", x, y, w, h);
 }
 
-void DrawPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b) {
-  if (!ctx.as<bool>()) return;
+inline void DrawPixel(int x, int y, uint8_t r, uint8_t g, uint8_t b) {
   val img = ctx.call<val>("getImageData", x, y, 1, 1);
   img["data"].set(0, r);
   img["data"].set(1, g);
@@ -32,5 +30,13 @@ inline int GetWidth() { return canvas["width"].as<int>(); }
 inline int GetHeight() { return canvas["height"].as<int>(); }
 
 inline void Clear() { DrawRect(0, 0, GetWidth(), GetHeight(), "black"); }
+
+inline val GetImageData() {
+  return ctx.call<val>("getImageData", 0, 0, GetWidth(), GetHeight());
+}
+
+inline void PutImageData(val image) {
+  ctx.call<void>("putImageData", image, 0, 0);
+}
 
 }  // namespace canvas
